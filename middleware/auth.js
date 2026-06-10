a5c449d81928833d9c01847ddfc4ebd620938d79
WENS-V4 <bywensoffby43@gmail.com>
2026-06-10 03:44:02 -0400

    Actualizar auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,47 @@
-u
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+const auth = async (req, res, next) => {
+    try {
+
+        const token =
+            req.headers.authorization?.split(" ")[1];
+
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: "Access denied"
+            });
+        }
+
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+        const user = await User.findById(
+            decoded.id
+        ).select("-password");
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+
+        req.user = user;
+
+        next();
+
+    } catch (error) {
+
+        res.status(401).json({
+            success: false,
+            message: "Invalid token"
+        });
+
+    }
+};
+
+export default auth;
