b24e370a487683b10f6ad98dfcdae1bc598fa715
WENS-V4 <bywensoffby43@gmail.com>
2026-06-10 03:32:17 -0400

    Actualizar deployService.js

--- services/deployService.js
+++ services/deployService.js
@@ -1,1 +1,35 @@
-h
+export const deployZip = async (req, res) => {
+    try {
+
+        res.json({
+            success: true,
+            message: "ZIP deployment started"
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+
+    }
+};
+
+export const deployGithub = async (req, res) => {
+    try {
+
+        res.json({
+            success: true,
+            message: "GitHub deployment started"
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+
+    }
+};
