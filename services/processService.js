ccb620e844056da350c9739c3d46062574c63bf4
WENS-V4 <bywensoffby43@gmail.com>
2026-06-10 03:33:02 -0400

    Actualizar processService.js

--- services/processService.js
+++ services/processService.js
@@ -1,1 +1,26 @@
-y
+export const startBot = async (req, res) => {
+
+    res.json({
+        success: true,
+        message: "Bot started"
+    });
+
+};
+
+export const stopBot = async (req, res) => {
+
+    res.json({
+        success: true,
+        message: "Bot stopped"
+    });
+
+};
+
+export const restartBot = async (req, res) => {
+
+    res.json({
+        success: true,
+        message: "Bot restarted"
+    });
+
+};
