254e9488a799b9b69cd4b28a419f3dde7878a936
WENS-V4 <bywensoffby43@gmail.com>
2026-06-10 03:33:52 -0400

    Actualizar credsService.js

--- services/credsService.js
+++ services/credsService.js
@@ -1,1 +1,18 @@
-y
+export const getLogs = async (req, res) => {
+
+    res.json({
+        success: true,
+        logs: []
+    });
+
+};
+
+export const getBotLogs = async (req, res) => {
+
+    res.json({
+        success: true,
+        botId: req.params.botId,
+        logs: []
+    });
+
+};
