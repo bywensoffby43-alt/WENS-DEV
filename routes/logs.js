659e027654e74dbbe7d605a4fee09665eb94afcf
WENS-V4 <bywensoffby43@gmail.com>
2026-06-10 03:30:59 -0400

    Actualizar logs.js

--- routes/logs.js
+++ routes/logs.js
@@ -1,1 +1,19 @@
-h
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    res.json({
+        success: true,
+        logs: []
+    });
+});
+
+router.get("/:botId", async (req, res) => {
+    res.json({
+        success: true,
+        botId: req.params.botId
+    });
+});
+
+export default router;
