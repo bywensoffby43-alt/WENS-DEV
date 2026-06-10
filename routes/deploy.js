d930b554fbe6a007dba22c64e8b9435e0e9c1b50
WENS-V4 <bywensoffby43@gmail.com>
2026-06-10 03:30:10 -0400

    Actualizar deploy.js

--- routes/deploy.js
+++ routes/deploy.js
@@ -1,1 +1,40 @@
-g
+import express from "express";
+
+const router = express.Router();
+
+router.post("/zip", async (req, res) => {
+    res.json({
+        success: true,
+        message: "ZIP Deploy Route"
+    });
+});
+
+router.post("/github", async (req, res) => {
+    res.json({
+        success: true,
+        message: "GitHub Deploy Route"
+    });
+});
+
+router.post("/start/:id", async (req, res) => {
+    res.json({
+        success: true,
+        message: "Bot Started"
+    });
+});
+
+router.post("/stop/:id", async (req, res) => {
+    res.json({
+        success: true,
+        message: "Bot Stopped"
+    });
+});
+
+router.post("/restart/:id", async (req, res) => {
+    res.json({
+        success: true,
+        message: "Bot Restarted"
+    });
+});
+
+export default router;
