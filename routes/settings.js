381be67549d659ea9739e07795cbd04983a71a95
WENS-V4 <bywensoffby43@gmail.com>
2026-06-10 03:31:20 -0400

    Actualizar settings.js

--- routes/settings.js
+++ routes/settings.js
@@ -1,1 +1,19 @@
-d
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    res.json({
+        success: true,
+        settings: {}
+    });
+});
+
+router.put("/", async (req, res) => {
+    res.json({
+        success: true,
+        message: "Settings Updated"
+    });
+});
+
+export default router;
