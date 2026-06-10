0f3186f0f6a07207ede0660cf15cfa5fcfd54719
WENS-V4 <bywensoffby43@gmail.com>
2026-06-10 03:33:28 -0400

    Actualizar uploadService.js

--- services/uploadService.js
+++ services/uploadService.js
@@ -1,1 +1,26 @@
-j
+export const uploadImage = async (req, res) => {
+
+    res.json({
+        success: true,
+        message: "Image uploaded"
+    });
+
+};
+
+export const uploadZip = async (req, res) => {
+
+    res.json({
+        success: true,
+        message: "ZIP uploaded"
+    });
+
+};
+
+export const uploadCreds = async (req, res) => {
+
+    res.json({
+        success: true,
+        message: "Creds uploaded"
+    });
+
+};
