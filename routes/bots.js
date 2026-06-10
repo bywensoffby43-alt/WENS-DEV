e325a27f07d33a9b064f26fdeb1b170b2aac6d8d
WENS-V4 <bywensoffby43@gmail.com>
2026-06-10 03:29:41 -0400

    Actualizar bots.js

--- routes/bots.js
+++ routes/bots.js
@@ -1,1 +1,39 @@
-h
+import express from "express";
+import Bot from "../models/Bot.js";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    const bots = await Bot.find();
+    res.json(bots);
+});
+
+router.get("/:id", async (req, res) => {
+    const bot = await Bot.findById(req.params.id);
+    res.json(bot);
+});
+
+router.post("/", async (req, res) => {
+    const bot = await Bot.create(req.body);
+    res.json(bot);
+});
+
+router.put("/:id", async (req, res) => {
+    const bot = await Bot.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+
+    res.json(bot);
+});
+
+router.delete("/:id", async (req, res) => {
+    await Bot.findByIdAndDelete(req.params.id);
+
+    res.json({
+        success: true
+    });
+});
+
+export default router;
