73643a7a31954b5baf9d620780964a56c2155026
WENS-V4 <bywensoffby43@gmail.com>
2026-06-12 23:46:46 -0400

    Actualizar files.js

--- routes/files.js
+++ routes/files.js
@@ -6,29 +6,24 @@ import {
     uploadCreds
 } from "../services/uploadService.js";
 
-import express from "express";
-
 const router = express.Router();
 
-router.post("/upload-image", async (req, res) => {
-    res.json({
-        success: true,
-        message: "Image Uploaded"
-    });
-});
+// Upload Image
+router.post(
+    "/upload-image",
+    uploadImage
+);
 
-router.post("/upload-zip", async (req, res) => {
-    res.json({
-        success: true,
-        message: "ZIP Uploaded"
-    });
-});
+// Upload ZIP
+router.post(
+    "/upload-zip",
+    uploadZip
+);
 
-router.post("/upload-creds", async (req, res) => {
-    res.json({
-        success: true,
-        message: "Creds Uploaded"
-    });
-});
+// Upload Creds
+router.post(
+    "/upload-creds",
+    uploadCreds
+);
 
 export default router;
