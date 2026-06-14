04194994deb041797baf45d3e7dec80ab3b45856
WENS-V4 <bywensoffby43@gmail.com>
2026-06-14 11:36:41 -0400

    Actualizar monitor.js

--- js/monitor.js
+++ js/monitor.js
@@ -2,18 +2,56 @@ async function loadBotStatus(name) {
 
     try {
 
-        const res = await fetch(
-            `/api/monitor/${name}`
-        );
+        const res =
+            await fetch(
+                `/api/monitor/${name}`
+            );
 
         const data =
             await res.json();
 
-        console.log(data);
+        document.getElementById(
+            "botStatus"
+        ).innerText =
+            data.online
+                ? "🟢 Online"
+                : "🔴 Offline";
+
+        document.getElementById(
+            "cpu"
+        ).innerText =
+            data.cpu;
+
+        document.getElementById(
+            "ram"
+        ).innerText =
+            (
+                data.memory /
+                1024 /
+                1024
+            ).toFixed(2);
+
+        document.getElementById(
+            "uptime"
+        ).innerText =
+            Math.floor(
+                (Date.now() -
+                data.uptime)
+                / 1000
+            ) + " sec";
 
     } catch (error) {
 
         console.error(error);
+
     }
 
 }
+
+setInterval(() => {
+
+    loadBotStatus("KING");
+
+}, 5000);
+
+loadBotStatus("KING");
