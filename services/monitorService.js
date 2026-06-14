67a1b6cb7c87ca53de18d7fc6489fc4fd09a911f
WENS-V4 <bywensoffby43@gmail.com>
2026-06-14 11:25:12 -0400

    Actualizar monitorService.js

--- services/monitorService.js
+++ services/monitorService.js
@@ -1,1 +1,50 @@
-bb
+import pm2 from "pm2";
+
+export const getBotStatus = (name) => {
+    return new Promise((resolve, reject) => {
+
+        pm2.connect((err) => {
+
+            if (err) {
+                return reject(err);
+            }
+
+            pm2.describe(name, (err, process) => {
+
+                pm2.disconnect();
+
+                if (err) {
+                    return reject(err);
+                }
+
+                if (!process || process.length === 0) {
+                    return resolve({
+                        online: false,
+                        cpu: 0,
+                        memory: 0,
+                        uptime: 0
+                    });
+                }
+
+                const bot = process[0];
+
+                resolve({
+                    online:
+                        bot.pm2_env.status === "online",
+
+                    cpu:
+                        bot.monit.cpu || 0,
+
+                    memory:
+                        bot.monit.memory || 0,
+
+                    uptime:
+                        bot.pm2_env.pm_uptime || 0
+                });
+
+            });
+
+        });
+
+    });
+};
