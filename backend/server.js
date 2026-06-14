43ad91e10d27a963cf2eb1d9920183ec787e387a
WENS-V4 <bywensoffby43@gmail.com>
2026-06-14 11:29:12 -0400

    Actualizar server.js

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,10 @@ const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+    "/api/monitor",
+    monitorRoutes
+);
 
 // Frontend
 app.use(
