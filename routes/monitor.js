15849b44013c81205e4023b7bf2878d5f138b78c
WENS-V4 <bywensoffby43@gmail.com>
2026-06-14 11:25:59 -0400

    Actualizar monitor.js

--- routes/monitor.js
+++ routes/monitor.js
@@ -1,1 +1,31 @@
-bb
+import express from "express";
+import { getBotStatus } from "../services/monitorService.js";
+
+const router = express.Router();
+
+router.get("/:name", async (req, res) => {
+
+    try {
+
+        const status =
+            await getBotStatus(
+                req.params.name
+            );
+
+        res.json({
+            success: true,
+            ...status
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+
+    }
+
+});
+
+export default router;
