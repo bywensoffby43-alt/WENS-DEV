8156c7f4d623756c0e26b6105b7917e4e0dca292
WENS-V4 <bywensoffby43@gmail.com>
2026-06-14 11:28:01 -0400

    Actualizar monitor.js

--- js/monitor.js
+++ js/monitor.js
@@ -1,1 +1,19 @@
-bb
+async function loadBotStatus(name) {
+
+    try {
+
+        const res = await fetch(
+            `/api/monitor/${name}`
+        );
+
+        const data =
+            await res.json();
+
+        console.log(data);
+
+    } catch (error) {
+
+        console.error(error);
+    }
+
+}
